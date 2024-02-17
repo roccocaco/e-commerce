3b4207893012bcb4ac6c2a160ca976401e4aa5d4
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-02-17 10:53:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 import { fetchApi } from "./fetchApi.js"
 
+async function main() {
+  const resultado = await fetchApi()
+  console.log(resultado)
+  
+}
 
+
+window.addEventListener("load", main)
