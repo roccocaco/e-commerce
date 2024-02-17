b72f8adfb3aa7b197a29233c760e9e4d4b5146f0
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-02-17 11:19:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 import { fetchApi } from "./fetchApi.js"
+const resultados = document.getElementById('resultados');
+
+function createElements(array) {
+  array.map((e) => resultados.innerHTML += `
+    <div>
+      <img src = "${e.thumbnail}">
+    </div>
+  `)
+}
 
 async function main() {
   const resultado = await fetchApi()
-  console.log(resultado)
-  
+  createElements(resultado.results)
 }
 
 
