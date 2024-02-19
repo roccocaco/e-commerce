30e34ba68b651cdbd460550b60cef83e87f7d493
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-02-19 11:24:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const resultados = document.getElementById('resultados');
 
 function createElements(array) {
   array.map((e) => resultados.innerHTML += `
-    <div>
+    <div class="col-md-4 col-12 mb-5">
+    <div class="d-flex justify-content-center">
       <img src = "${e.thumbnail}">
     </div>
+
+    <div>
+    <p>${e.title}</p>
+    <h5>R$ ${e.price}</h5>
+    </div>
+    </div>
+
   `)
 }
 
