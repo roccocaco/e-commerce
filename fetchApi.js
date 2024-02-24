1aad63a464ba9a65128e23fe45cff8421d2743d3
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-02-24 10:45:05 -0300

    Add files via upload

--- fetchApi.js
+++ fetchApi.js
@@ -1,6 +1,10 @@
 export async function fetchApi() {
-  const req = await fetch("https://api.mercadolibre.com/sites/MLB/search?q=teclado%20musical")
-  const res = await req.json()
+  const req1 = await fetch("https://api.mercadolibre.com/sites/MLB/search?q=teclado")
+  const res1 = await req1.json()
 
-  return res
+  const req2 = await fetch("https://api.mercadolibre.com/sites/MLB/search?q=piano");
+  const res2 = await req2.json();
+
+  const req3 = await fetch("https://api.mercadolibre.com/sites/MLB/search?q=bateria");
+  const res3 = await req3.json();
 }
